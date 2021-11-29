e726ed4fa626be219961d2a60706d32612ffcb16
Eitheldan <eithel57@gmail.com>
2021-11-29 18:44:45 +0100

    Symfony power

---
+++ src/Controller/AdminSecuController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminSecuController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function index(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $encoder): Response
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(InscriptionType::class, $utilisateur);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $passwordCrypte = $encoder->hashPassword($utilisateur, $utilisateur->getPassword());
+            $utilisateur->setPassword($passwordCrypte);
+            $em->persist($utilisateur);
+            $em->flush();
+            return $this->redirectToRoute("Films");
+        }
+
+        return $this->render('admin_secu\inscription.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
